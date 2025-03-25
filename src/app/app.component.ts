4621cda27f7ff1382a93047c4b69c19b9cf1fc5d
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-03-25 16:55:48 +0200

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared-ui/navbar/navbar.component";
+import { FooterComponent } from "./shared-ui/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   schemas:  [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
