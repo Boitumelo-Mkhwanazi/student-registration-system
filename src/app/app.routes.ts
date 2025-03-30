c93afe73652557c8fdeca86443b16e482d80174c
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-03-30 22:41:42 +0200

    Sign in and up pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { FooterComponent } from './shared-ui/footer/footer.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 export const routes: Routes = [
     {
@@ -19,7 +20,11 @@ export const routes: Routes = [
         component: ContactUsComponent,
     },
     {
-        path: 'sign-in',
+        path: 'sign-up',
         component: SignInComponent,
+    },
+    {
+        path: 'log-in',
+        component: LogInComponent,
     }
 ];
--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
