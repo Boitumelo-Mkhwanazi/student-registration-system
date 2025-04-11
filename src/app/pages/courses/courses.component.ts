ba897ca6e6c517eb561bdd5a52cf67e6d6bcd64d
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-11 23:08:47 +0200

    Course component

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from "../../shared-ui/navbar/navbar.component";
+import { CourseComponent } from "./course/course.component";
 
 @Component({
   selector: 'app-courses',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CourseComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
