cf1946330e9972ac06060e12f7e101736d54633c
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-16 20:26:22 +0200

    Featured courses section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,16 +5,23 @@ import { HomeService } from '../../services/pages/home.service';
 import { CardStepComponent } from "./components/card-step/card-step.component";
 import { NavbarComponent } from '../../shared-ui/navbar/navbar.component';
 import { FooterComponent } from '../../shared-ui/footer/footer.component';
+import { CourseService } from '../../services/pages/courses.service';
+import { CourseComponent } from '../courses/components/course/course.component';
 
 @Component({
   selector: 'app-home',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CardManageComponent, CardStepComponent, NavbarComponent, FooterComponent]
+  imports: [CardManageComponent, CardStepComponent, NavbarComponent, FooterComponent, CourseComponent]
 })
 export class HomeComponent {
   homeService = inject(HomeService);
+  coursesData = inject(CourseService);
+
+  get coursesArray() {
+    return this.coursesData.courses;
+  }
 
   get cardsArray() {
     return this.homeService.cardsArray;
