16da4af479e5f0f4a1eae056d4be20ec53557901
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-17 23:36:08 +0200

    Course-details 1

--- src/app/pages/courses/course-details/course-details.component.ts
+++ src/app/pages/courses/course-details/course-details.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject, input, signal } from '@angular/core';
 import { NavbarComponent } from "../../../shared-ui/navbar/navbar.component";
+import { CourseService } from '../../../services/pages/courses.service';
 
 @Component({
   selector: 'app-course-details',
   imports: [NavbarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css'
 })
 export class CourseDetailsComponent {
+  courseName = input.required<string>();
+  showingMore = signal<boolean>(false);
+  private courseSerivice = inject(CourseService)
 
+  selectedCourse = computed(
+    () => this.courseSerivice.courses.find((course) => course.title === this.courseName())!
+  );
+
+  onShowMore() {
+    this.showingMore.set(!this.showingMore())
+  }
 }
