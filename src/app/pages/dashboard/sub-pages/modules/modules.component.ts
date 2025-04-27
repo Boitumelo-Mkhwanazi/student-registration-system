9f13a4e1a06ce15e40d58c3d1a135ca3947ca995
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-27 14:46:47 +0200

    Dashboard Page

--- src/app/pages/dashboard/sub-pages/modules/modules.component.ts
+++ src/app/pages/dashboard/sub-pages/modules/modules.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CourseService } from '../../../../services/shared-services/courses.service';
+import { CourseComponent } from "../../../../shared-ui/courses/course/course.component";
 
 @Component({
   selector: 'app-modules',
-  imports: [],
+  imports: [CourseComponent],
   templateUrl: './modules.component.html',
   styleUrl: './modules.component.css'
 })
 export class ModulesComponent {
+  private coursesService = inject(CourseService);
 
+  get coursesArray() {
+    return this.coursesService.courses;
+  }
 }
