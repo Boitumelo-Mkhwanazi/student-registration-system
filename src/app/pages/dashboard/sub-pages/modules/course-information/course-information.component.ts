b2f0587576cf78a26433423016d60ada00a534ed
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-27 16:39:02 +0200

    Selected Course Page

--- src/app/pages/dashboard/sub-pages/modules/course-information/course-information.component.ts
+++ src/app/pages/dashboard/sub-pages/modules/course-information/course-information.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject, input, signal } from '@angular/core';
+import { CourseService } from '../../../../../services/shared-services/courses.service';
+import { CourseDetailsComponent } from "../../../../../shared-ui/courses/course-details/course-details.component";
+import { CourseComponent } from '../../../../../shared-ui/courses/course/course.component';
 
 @Component({
   selector: 'app-course-information',
-  imports: [],
+  imports: [CourseComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './course-information.component.html',
   styleUrl: './course-information.component.css'
 })
 export class CourseInformationComponent {
+  courseName = input.required<string>();
+  showingMore = signal<boolean>(false);
+  private courseSerivice = inject(CourseService);
+  private coursesData = inject(CourseService);
 
+  get coursesArray() {
+    return this.coursesData.courses;
+  }
+
+  selectedCourse = computed(
+    () => this.courseSerivice.courses.find((course) => course.title === this.courseName())!
+  );
+
+  onShowMore() {
+    this.showingMore.set(!this.showingMore())
+  }
 }
