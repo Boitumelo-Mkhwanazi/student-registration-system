12c2a6a96fbcf9f33798842c9d25225dad933439
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-04-30 00:15:46 +0200

    Overview page 1/2

--- src/app/pages/dashboard/sub-pages/overview/overview.component.ts
+++ src/app/pages/dashboard/sub-pages/overview/overview.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
